f129f8c0c42a4edac23b1e0c0fb61bb32a97c51f
david <davidliat98@gmail.com>
2025-06-16 01:06:18 -0400

    updated styling

--- js/navbar-darken-on-scroll.js
+++ js/navbar-darken-on-scroll.js
@@ -1,70 +1,64 @@
-import utils from "./utils";
+import utils from './utils'
 /* eslint-disable */
 /* -------------------------------------------------------------------------- */
 /*                         Navbar Darken on scroll                        */
 /* -------------------------------------------------------------------------- */
 const navbarDarkenOnScroll = () => {
   const Selector = {
-    NAVBAR: "[data-navbar-darken-on-scroll]",
-    NAVBAR_COLLAPSE: ".navbar-collapse",
-    NAVBAR_TOGGLER: ".navbar-toggler",
-  };
+    NAVBAR: '[data-navbar-darken-on-scroll]',
+    NAVBAR_COLLAPSE: '.navbar-collapse',
+    NAVBAR_TOGGLER: '.navbar-toggler',
+  }
   // const ClassNames = {
   //   COLLAPSED: "collapsed",
   // };
-  
 
   const Events = {
-    SCROLL: "scroll",
-    SHOW_BS_COLLAPSE: "show.bs.collapse",
-    HIDE_BS_COLLAPSE: "hide.bs.collapse",
-    HIDDEN_BS_COLLAPSE: "hidden.bs.collapse",
-  };
+    SCROLL: 'scroll',
+    SHOW_BS_COLLAPSE: 'show.bs.collapse',
+    HIDE_BS_COLLAPSE: 'hide.bs.collapse',
+    HIDDEN_BS_COLLAPSE: 'hidden.bs.collapse',
+  }
   const DataKey = {
-    NAVBAR_DARKEN_ON_SCROLL: "navbar-darken-on-scroll",
-  };
-  const navbar = document.querySelector(Selector.NAVBAR);
+    NAVBAR_DARKEN_ON_SCROLL: 'navbar-darken-on-scroll',
+  }
+  const navbar = document.querySelector(Selector.NAVBAR)
   if (navbar) {
-    const defaultColorName = 'dark';
-    const windowHeight = window.innerHeight;
-    const html = document.documentElement;
+    const defaultColorName = 'dark'
+    const windowHeight = window.innerHeight
+    const html = document.documentElement
     // const navbarCollapse = navbar.querySelector(Selector.NAVBAR_COLLAPSE);
-    const allColors = { ...utils.colors, ...utils.grays };
-    const name = utils.getData(navbar, DataKey.NAVBAR_DARKEN_ON_SCROLL);
+    const allColors = { ...utils.colors, ...utils.grays }
+    const name = utils.getData(navbar, DataKey.NAVBAR_DARKEN_ON_SCROLL)
     const colorName = Object.keys(allColors).includes(name.toString())
       ? name
-      : defaultColorName;
-    const color = allColors[colorName];
+      : defaultColorName
+    const color = allColors[colorName]
     // const bgClassName = `bg-${colorName}`;
-    const colorRgb = utils.hexToRgb(color);
-    const { backgroundImage } = window.getComputedStyle(navbar);
+    const colorRgb = utils.hexToRgb(color)
+    const { backgroundImage } = window.getComputedStyle(navbar)
     // const transition = "background-color 0.35s ease";
     const borderColor = utils.hexToRgb(allColors['700'])
-    const paddingTop = 48;
-    navbar.style.paddingTop = `${paddingTop}px`;
-    navbar.style.backgroundImage = "none";
-    navbar.style.borderBottom = "none";
+    const paddingTop = 48
+    navbar.style.paddingTop = `${paddingTop}px`
+    navbar.style.backgroundImage = 'none'
+    navbar.style.borderBottom = 'none'
     //shadow
     const shadowName = 'shadow-transition'
     // Change navbar background color on scroll'
     window.addEventListener(Events.SCROLL, () => {
-      const { scrollTop } = html;
-      let alpha = (scrollTop / windowHeight) * 2;
-      alpha >= 1 && (alpha = 1);
-      navbar.style.backgroundColor = `rgba(${colorRgb[0]}, ${colorRgb[1]}, ${colorRgb[2]}, ${alpha})`;
-      navbar.style.borderBottom = `1px solid rgba(${borderColor[0]}, ${borderColor[1]}, ${borderColor[2]}, ${alpha})`;
-      navbar.style.paddingTop = `${paddingTop * (1 - alpha)}px`;
-      navbar.style.backgroundImage =
-        alpha > 0 
-          ? backgroundImage
-          : "none";
-      alpha > 0.2 ? navbar.classList.add(shadowName):navbar.classList.remove(shadowName);
-    });
-    
-    
-    
-    
-    
+      const { scrollTop } = html
+      let alpha = (scrollTop / windowHeight) * 2
+      alpha >= 1 && (alpha = 1)
+      navbar.style.backgroundColor = `rgba(${colorRgb[0]}, ${colorRgb[1]}, ${colorRgb[2]}, ${alpha})`
+      navbar.style.borderBottom = `1px solid rgba(${borderColor[0]}, ${borderColor[1]}, ${borderColor[2]}, ${alpha})`
+      navbar.style.paddingTop = `${paddingTop * (1 - alpha)}px`
+      navbar.style.backgroundImage = alpha > 0 ? backgroundImage : 'none'
+      alpha > 0.2
+        ? navbar.classList.add(shadowName)
+        : navbar.classList.remove(shadowName)
+    })
+
     // Toggle bg class on window resize
     // utils.resize(() => {
     //   const breakPoint = utils.getBreakpoint(navbar);
@@ -106,5 +100,5 @@ const navbarDarkenOnScroll = () => {
     //   navbar.style.transition = "none";
     // });
   }
-};
-export default navbarDarkenOnScroll;
+}
+export default navbarDarkenOnScroll
